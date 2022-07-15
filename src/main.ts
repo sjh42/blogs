9b216196c2e24b5a5d78975f0c084c17135c0a30
sjh <sjh990503@163.com>
2022-07-15 22:52:23 +0800

    chore: update deps

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 import { ViteSSG } from 'vite-ssg'
-import routes from 'virtual:generated-pages'
+import autoRoutes from 'pages-generated'
+import NProgress from 'nprogress'
+import dayjs from 'dayjs'
+import LocalizedFormat from 'dayjs/plugin/localizedFormat.js'
 import App from './App.vue'
 
 import '@unocss/reset/tailwind.css'
 import './styles/main.css'
 import 'uno.css'
+
+const routes = autoRoutes.map((i) => {
+  return {
+    ...i,
+    alias: i.path.endsWith('/')
+      ? `${i.path}index.html`
+      : `${i.path}.html`,
+  }
+})
 
 const scrollBehavior = (to: any, from: any, savedPosition: any) => {
   if (savedPosition)
@@ -13,4 +25,11 @@ const scrollBehavior = (to: any, from: any, savedPosition: any) => {
     return { top: 0 }
 }
 
-export const createApp = ViteSSG(App, { routes, scrollBehavior })
+export const createApp = ViteSSG(App, { routes, scrollBehavior },  ({ router, isClient }) => {
+	dayjs.extend(LocalizedFormat)
+
+	if (isClient) {
+		router.beforeEach(() => { NProgress.start() })
+		router.afterEach(() => { NProgress.done() })
+	}
+},)
